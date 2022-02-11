03860d89639bc4cd546191284373b04f9750930d
diegomtzcdo <97270833+diegomtzcdo@users.noreply.github.com>
2022-02-11 08:55:43 -0600

    Correcion Delete User

--- Scripts/DeleteUserSP.sql
+++ Scripts/DeleteUserSP.sql
@@ -3,6 +3,10 @@ GO
 CREATE or ALTER PROCEDURE DeleteUserSP (@oldMail VARCHAR(255))
 AS
 BEGIN
+	DECLARE @idUsr int;
+	SET @idUsr = ( SELECT id_usuario FROM [kokonutPrueba].[dbo].[usuarios] WHERE [email] = @oldMail);
+	DELETE FROM [kokonutPrueba].[dbo].[usuarios_roles] WHERE usuario_id_usuario = @idUsr;
+	DELETE FROM [kokonutPrueba].[dbo].[usuarios_fotos] WHERE usuario_id_usuario = @idUsr;
 	DELETE FROM [kokonutPrueba].[dbo].[usuarios]
 	WHERE [email] = @oldMail;
 END
